8125e1b1a0281c52fa0b178aa5bbd932aaf05652
hugorosa23 <hugo.rosa2304@gmail.com>
2024-02-02 14:42:54 -0300

    feat: added imageUrl to services

--- prisma/migrations/20240202172316_add_service_image_url/migration.sql
+++ prisma/migrations/20240202172316_add_service_image_url/migration.sql
@@ -3,8 +3,12 @@
 
   - You are about to drop the column `imageURL` on the `Barbershop` table. All the data in the column will be lost.
   - Added the required column `imageUrl` to the `Barbershop` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `imageUrl` to the `Service` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Barbershop" DROP COLUMN "imageURL",
 ADD COLUMN     "imageUrl" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "imageUrl" TEXT NOT NULL;
