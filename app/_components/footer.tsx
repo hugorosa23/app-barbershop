fc624fc99a1a5f6a45ca067311c5ac4f0553ec43
hugorosa23 <hugo.rosa2304@gmail.com>
2024-02-13 19:17:34 -0300

    feat: Ensure booking can be canceled.

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
-    return ( 
-        <div className="w-full bg-card py-6 px-5">
+    return (        
+        <div className="fixed bottom-0 left-0 right-0 w-full bg-card py-6 px-5">
             <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Todos os Direitos Reservados. Hugo Rosa</p>
-        </div> 
+        </div>
     );
 }
  
