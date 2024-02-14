905465984ad8490d4ae57618ee7fa3eac9fc4f2d
hugorosa23 <hugo.rosa2304@gmail.com>
2024-02-14 00:55:34 -0300

    feat: ensure user can search for barbershops

---
+++ app/barbershops/page.tsx
@@ -0,0 +1,46 @@
+import { redirect } from "next/navigation";
+import BarbershopItem from "../(home)/_components/barbershop-item";
+import Header from "../_components/header";
+import { db } from "../_lib/prisma";
+
+interface BarbershopsPageProps {
+  searchParams: {
+    search?: string;
+  };
+}
+
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+  if (!searchParams.search) {
+    return redirect("/");
+  }
+
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams.search,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h1 className="front-gray-400 font-bold text-xs uppercase">
+          Resultados para &quot;{searchParams.search}&quot;
+        </h1>
+
+        <div className="grid grid-cols-2 mt-3 gap-4">
+          {barbershops.map((barbershop) => (
+            <div className="w-full" key={barbershop.id}>
+              <BarbershopItem barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BarbershopsPage;
