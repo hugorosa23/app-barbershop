406c22be7233eb5943484e66399a557f7df31ef7
hugorosa23 <hugo.rosa2304@gmail.com>
2024-02-14 01:37:26 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (        
-        <div className="w-full bg-card py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Todos os Direitos Reservados. Hugo Rosa</p>
-        </div>
-    );
-}
- 
+  return (
+    <footer className="w-full bg-card py-6 px-5">
+      <p className="text-gray-400 text-xs font-bold opacity-75">
+        © 2024 Todos os Direitos Reservados. Hugo Rosa
+      </p>
+    </footer>
+  );
+};
+
 export default Footer;
